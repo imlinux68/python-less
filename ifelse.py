349de9e69956ce3f94a0d4bc952270b8d5fcfdb4
imlinux68 <imlinux68@gmail.com>
2023-09-18 22:30:17 +0300

    added if else choice

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+#!/bin/python
+
+def choice():
+    country = input("Enter a country name: ")
+    if country == "America":
+        print("Hello America")
+    else:
+        print("Hello, Canada!")
+
+choice()
